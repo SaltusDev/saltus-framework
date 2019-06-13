95fbea3d8a588a1beb12e933ece110269a83ad77
Pedro de Carvalho <p@43.lc>
2019-06-13 18:46:11 +0100

    Create CMB2 Fields Service

---
+++ src/Fields/CMB2Fields.php
@@ -0,0 +1,66 @@
+<?php
+namespace Saltus\WP\Framework\Fields;
+
+final class CMB2Fields {
+
+	private $name;
+	private $meta;
+
+
+	/**
+	 * Instantiate the CMB2 Fields object.
+	 *
+	 */
+	public function __construct() {
+
+	}
+
+	public function setup( $name, $meta ) {
+
+		$this->name = $name;
+		$this->meta = $meta;
+	}
+
+	public function init() {
+
+		/**
+		 * Create Fields
+		 *
+		 * Start by setting up Sections, which in turn will create fields
+		*/
+		foreach ( $this->meta as $section ) {
+			if ( ! empty( $section['fields'] ) ) {
+				$this->create_section( $section );
+				continue;
+			}
+			//TODO by pcarvalho: can meta have only fields without a section?
+		}
+	}
+
+	private function create_section( $section ) {
+		$args = array(
+			'id'           => $section['id'],
+			'title'        => $section['title'],
+			'object_types' => $this->name,
+			'context'      => 'normal',
+			'priority'     => 'high',
+			'show_names'   => true,
+		);
+
+		$cmb = \new_cmb2_box( $args );
+		foreach ( $section['fields'] as $id => $fields ) {
+			$this->create_field( $cmb, $id, $fields );
+		}
+	}
+
+	private function create_field( $cmb, $id, $field ) {
+		$args = array(
+			'id'   => $id,
+			'name' => $field['name'],
+			'desc' => $field['description'],
+			'type' => $field['type'],
+		);
+		$cmb->add_field( $args );
+	}
+
+}
---
+++ src/Fields/Service.php
@@ -0,0 +1,48 @@
+<?php
+namespace Saltus\WP\Framework\Fields;
+
+use Saltus\WP\Framework\Infrastructure\{
+	Conditional,
+	ServiceInterface
+};
+
+/**
+ * This sample service only renders a silly "Hello World" notice in the admin
+ * backend.
+ *
+ * It is meant to illustrate how to hook services into the plugin flow
+ * and how to have their dependencies by injected.
+ *
+ * Note that the dependency here is actually an interface, not a class. We can
+ * still just transparently use it though.
+ */
+final class Service implements ServiceInterface, Conditional {
+
+	/**
+	 * Check whether the conditional service is currently needed.
+	 *
+	 * @return bool Whether the conditional service is needed.
+	 */
+	public static function is_needed(): bool {
+		/*
+		 * We only load this sample service on the admin backend.
+		 * If this conditional returns false, the service is never even
+		 * instantiated.
+		 */
+		return true;
+	}
+
+	/**
+	 * Instantiate a Service object.
+	 *
+	 */
+	public function __construct() {
+
+	}
+
+	public function get_new() {
+		return new CMB2Fields();
+	}
+
+}
+
