de750734f44963cf66b2030450acef188026fa1a
carmoreira <carlostrickster@gmail.com>
2019-08-30 01:56:49 +0200

    improved fields services

--- src/Fields/CodestarFields.php
+++ src/Fields/CodestarFields.php
@@ -25,17 +25,16 @@ final class CodestarFields {
 	public function init() {
 
 		/**
-		 * Create Fields
-		 *
-		 * Start by setting up Sections, which in turn will create fields
-		*/
-		/*foreach ( $this->meta as $section ) {
-			if ( empty( $section['fields'] ) ) {
+		 * Create Metaboxes
+		 */
+		foreach ( $this->meta as $box_id => $box ) {
+			if ( empty( $box['fields'] ) && empty( $box['sections'] ) ) {
 				continue;
 			}
-			$this->create_section( $section );
-			// TODO by pcarvalho: can meta have only fields without a section?
-		}*/
+
+			// else add just the fields
+			$this->create_metabox( $box_id, $box );
+		}
 
 		/**
 		 * Create Settings pages
@@ -44,10 +43,19 @@ final class CodestarFields {
 			if ( empty( $settings_page['fields'] ) && empty( $settings_page['sections'] ) ) {
 				continue;
 			}
+
+			// else add just the fields
 			$this->create_settings_page( $settings_id, $settings_page );
 		}
 	}
 
+	/**
+	 * Create settings page
+	 *
+	 * @param int   $settings_id identifier of the settings
+	 * @param array $settings_page paramaters for the page
+	 * @return void
+	 */
 	private function create_settings_page( $settings_id, $settings_page ) {
 
 		$default_args = array(
@@ -63,23 +71,94 @@ final class CodestarFields {
 		\CSF::createOptions( $settings_id, $args );
 
 		if ( isset( $settings_page['fields'] ) ) {
+			$settings_page['fields'] = $this->prepare_fields( $settings_page['fields'] );
 			$this->create_section( $settings_id, $settings_page );
 			return;
 		}
 
 		if ( isset( $settings_page['sections'] ) ) {
 			foreach ( $settings_page['sections'] as $section ) {
+				$section['fields'] = $this->prepare_fields( $section['fields'] );
 				$this->create_section( $settings_id, $section );
 			}
 		}
 
 	}
 
-	private function create_section( $settings_id, $section ) {
+	/**
+	 * Create metabox
+	 *
+	 * @param int   $box_id identifier of the metabox
+	 * @param array $box_settings paramaters for the page
+	 * @return void
+	 */
+	private function create_metabox( $box_id, $box_settings ) {
 
-		\CSF::createSection( $settings_id, $section );
-		return;
+		$default_args = array(
+			'post_type' => $this->name,
+			'priority'  => 'high',
+			'context'   => 'normal',
+			'theme'     => 'light',
+			'data_type' => 'unserialize',
+		);
 
+		$args = array_merge( $default_args, $box_settings );
+
+		// Create options
+		\CSF::createMetabox( $box_id, $args );
+
+		if ( isset( $box_settings['fields'] ) ) {
+
+			$box_settings['fields'] = $this->prepare_fields( $box_settings['fields'] );
+
+			$this->create_section( $box_id, $box_settings );
+			return;
+		}
+
+		if ( isset( $box_settings['sections'] ) ) {
+			foreach ( $box_settings['sections'] as $section ) {
+
+				$section['fields'] = $this->prepare_fields( $section['fields'] );
+				$this->create_section( $box_id, $section );
+			}
+		}
+
+	}
+
+	/**
+	 * Create section using builtin Codestart method
+	 *
+	 * @param string $id - identifier of the section
+	 * @param array  $section - parameters for the section
+	 * @return void
+	 */
+	private function create_section( $id, $section ) {
+
+		\CSF::createSection( $id, $section );
+
+	}
+
+	/**
+	 * Prepare fields to make sure they have all necessary parameters
+	 *
+	 * @param array $fields
+	 * @return array $fields array of fields prepared to be rendered by CodestarFields
+	 */
+	private function prepare_fields( $fields ) {
+
+		foreach ( $fields as $key => &$field ) {
+
+			$fields[ $key ]['id'] = $key;
+
+			if ( isset( $field['fields'] ) ) {
+
+				$field['fields'] = $this->prepare_fields( $field['fields'] );
+
+			}
+
+		}
+
+		return $fields;
 	}
 
 
