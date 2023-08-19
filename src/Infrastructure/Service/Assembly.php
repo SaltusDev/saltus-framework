f46255c9ac2a73855f0d8c530a3d0ef3b5af29ce
Pedro de Carvalho <p@43.lc>
2023-08-19 11:58:18 +0100

    Add assembly interface

---
+++ src/Infrastructure/Service/Assembly.php
@@ -0,0 +1,19 @@
+<?php
+namespace Saltus\WP\Framework\Infrastructure\Service;
+
+/**
+ * A conceptual service.
+ *
+ * Splitting your logic up into independent services makes the approach of
+ * assembling a plugin more systematic and scalable and lowers the cognitive
+ * load when the code base increases in size.
+ */
+interface Assembly {
+
+	/**
+	 * Create a new instance of the service provider
+	 *
+	 * @return object The new instance
+	 */
+	public static function make( $name, $project, $args );
+}
