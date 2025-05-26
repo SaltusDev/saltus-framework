5b1dbe3124b70f74c4c03c16219ad47417e5f1c2
Pedro de Carvalho <p@43.lc>
2025-05-26 17:18:00 +0900

    Add Has assets classes

---
+++ src/Infrastructure/Services/Assets/AssetsContainer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Saltus\WP\Framework\Infrastructure\Services\Assets;
+
+use Saltus\WP\Framework\Infrastructure\Service\Service;
+use Saltus\WP\Framework\Infrastructure\Container\SimpleContainer;
+
+class AssetsContainer extends SimpleContainer implements Service {
+
+	/**
+	 * Get all elements stored in the container.
+	 *
+	 * @return array
+	 */
+	public function getAll(): array {
+		return $this->getArrayCopy();
+	}
+}
