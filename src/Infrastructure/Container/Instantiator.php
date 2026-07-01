a5cc57a79bfad58b4444121397f8532f673bc389
Pedro de Carvalho <p@43.lc>
2026-07-02 02:13:29 +0800

    infra(container): fix phpdoc array type annotations
    Change phpdoc array type from array<int, mixed> to array<mixed> for interface and service container methods. This removes unnecessary integer-key constraint from generic array parameters.

--- src/Infrastructure/Container/Instantiator.php
+++ src/Infrastructure/Container/Instantiator.php
@@ -12,8 +12,8 @@ interface Instantiator {
 	/**
 	 * Make an object instance out of an interface or class.
 	 *
-	 * @param class-string     $target_class Class to make an object instance out of.
-	 * @param array<int, mixed> $dependencies Optional. Dependencies of the class.
+	 * @param class-string $target_class Class to make an object instance out of.
+	 * @param array<mixed> $dependencies Optional. Dependencies of the class.
 	 * @return object Instantiated object.
 	 */
 	public function instantiate( string $target_class, array $dependencies = [] ): object;
